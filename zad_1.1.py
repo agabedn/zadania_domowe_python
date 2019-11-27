333b5fd1fd60c5e8c1c108407e0c853d1849f2a0
Agnieszka Bednaruk <warda.aga@gmail.com>
2019-11-27 17:26:26 +0100

    Zadanie 1.1

---
+++ zad_1.1.py
@@ -0,0 +1,7 @@
+"""
+Napisz program, który prosi użytkownika (przez `input()`), żeby podał,
+ile kosztuje kilo ziemniaków. Niech program policzy i wyświetli,
+ile trzeba będzie zapłacić za pięć kilo ziemniaków.
+"""
+kilo_ziemniakow = int(input('Ile kosztuje 1 kg ziemniakow: '))
+print(f'Za 5 kg ziemniaków trzeba będzie zapłacic: {5 * kilo_ziemniakow} zł')
