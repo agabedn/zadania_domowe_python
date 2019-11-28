d7f600b1c210b85883dbb33e52ded735f381ba66
Agnieszka Bednaruk <warda.aga@gmail.com>
2019-11-28 08:46:41 +0100

    zadanie 1.9 - korekta

--- zad_1.9.py
+++ zad_1.9.py
@@ -2,13 +2,12 @@
 Napisz program, który wyświetla liczby od 1 do 100. Dla liczb podzielnych przez 3 niech program wyświetli `Fizz`;
 dla liczb podzielnych przez 5 niech wyświetli `Buzz`; a dla liczb podzielnych przez 15 niech wyświetli `FizzBuzz`.
 """
-i=1
-while i <= 100:
+for i in range(1, 101):
     print(i)
-    i+=1
-    if i >= 3 and i % 3 == 0:
-        print(f'Fizz {i}')
-    elif i >= 5 and i % 5 == 0:
-        print(f'Buzz {i}')
-    elif i >=15 and i % 15 == 0:
-        print(f'FizzBuzz {i}')
+    if i % 3 == 0:
+        print(f'Fizz')
+    if i % 5 == 0:
+        print('Buzz')
+    if i % 15 == 0:
+        print('FizzBuzz')
+    i += 1
